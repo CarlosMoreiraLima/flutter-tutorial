4cff9aa54df3b2df2fbf2f408c592197b112ef6a
Carlos Henrique <chmlima201@gmail.com>
2023-07-23 23:52:47 -0300

    Tutorial(Stack): Add background and mask color

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,62 +11,103 @@ class _LoginPageState extends State<LoginPage> {
   String usermail = "";
   String password = "";
 
+  Widget _body() {
+    return Center(
+      child: Container(
+        width: (MediaQuery.of(context).size.width / 10) * 9,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30.0),
+              child: Image.asset(
+                "assets/images/shiftpower.webp",
+                height: 150.0,
+                width: 150.0,
+              ),
+            ),
+            Container(
+              height: 50,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(55),
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.white,
+                  ),
+                ),
+                label: Text(
+                  "Email",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 24),
+              onChanged: (text) {
+                usermail = text;
+              },
+            ),
+            Container(height: 15),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(55),
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.white,
+                  ),
+                ),
+                fillColor: Colors.white,
+                label: Text(
+                  "Password",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 24),
+              onChanged: (text) {
+                password = text;
+              },
+            ),
+            Container(height: 20),
+            ElevatedButton(
+              child: Text("Entrar"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {
+                print("Email: $usermail \nPassword: $password");
+                Navigator.of(context).pushReplacementNamed('/home');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
-        body: SizedBox(
+        body: Stack(
           // width: double.infinity,
-          width: MediaQuery.of(context).size.width,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child: Image.asset(
-                  "assets/images/tinder.png",
-                  height: 150.0,
-                  width: 150.0,
-                ),
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "assets/images/background.jpg",
+                fit: BoxFit.cover,
               ),
-              Container(
-                height: 25,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text("Email"),
-                ),
-                onChanged: (text) {
-                  usermail = text;
-                },
-              ),
-              Container(height: 15),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text("Password"),
-                ),
-                onChanged: (text) {
-                  password = text;
-                },
-              ),
-              Container(height: 20),
-              ElevatedButton(
-                child: Text("Entrar"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                onPressed: () {
-                  print("Email: $usermail \nPassword: $password");
-                  Navigator.of(context).pushReplacementNamed('/home');
-                },
-              ),
-            ],
-          ),
+            ),
+            Container(
+              color: Colors.black.withOpacity(0.2),
+            ),
+            _body(),
+          ],
         ),
       ),
     );
